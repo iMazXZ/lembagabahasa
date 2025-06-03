d3c0046a99bd3aec599af5854e9e2a4df16b1ccc
iMazXZ <akungamemaulana@gmail.com>
2025-06-03 18:50:24 +0700

    mail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,15 @@ class AppServiceProvider extends ServiceProvider
     {
         \Carbon\Carbon::setLocale('id');
         PendaftaranEpt::observe(PendaftaranEptObserver::class);
+
+        // Disable SSL verification untuk mail SMTP
+        Config::set('mail.mailers.smtp.stream', [
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ]);
     }
     
 }
